dc772682d9c44fa9668e849be6849fbf7f04300c
dad <60132399+dad2vad@users.noreply.github.com>
2020-01-25 21:45:36 +0200

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -4,4 +4,14 @@ export default function About() {
       <p>This is the about page</p>
     </div>
   );
+  const Index = () => (
+  <div>
+<Link href="/about" title="About Page">
+  <a>About Page</a>
+</Link>
+    <p>Hello Next.js</p>
+  </div>
+);
 }
+
+
